c1ab995f97a7151ec8b63e5bbacdbe02bbd93cd1
pogermano <pogermano@gmail.com>
2021-12-08 10:42:17 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#heroesAPI
+projeto desenvolvido com base na disciplina ministrada por Kamila Santos no Bootcamp TQI Java Developer da DIO.me.
+API em SPRING WEBFLUX utilzando banco de dados dynamodb para crud de super herois.
+Testes unitários
+Actuator
+Swagger
---
+++ src/main/java/com/digitalinovationone/heroesapi/config/HeroesData.java
@@ -0,0 +1,13 @@
+package com.digitalinovationone.heroesapi.config;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+
+public class HeroesData {
+}
